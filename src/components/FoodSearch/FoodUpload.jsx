259a05f22943e072c0503eb5225595116b098c1c
Ji Yoon Park <jyp0293@gmail.com>
2021-11-22 12:50:49 +0900

    [#9] fixed: Add comments

--- src/components/FoodSearch/FoodUpload.jsx
+++ src/components/FoodSearch/FoodUpload.jsx
@@ -4,9 +4,12 @@ import PropTypes from 'prop-types';
 
 function FoodUpload({getImgUrl}){
 
+    // selectedFile : 사용자가 선택한 파일의 정보
+    // previewImgUrl : 사용자 로컬의 이미지를 프리뷰로 띄우기 위해 필요한 로컬 이미지 주소 값
     const [selectedFile, setSelectedFile] = useState(null);
     const [previewImgUrl, setPreviewImgUrl] = useState("");
 
+    // 프리뷰
     const preview = (e) => {
         const reader = new FileReader;
         const fileList = e.target.files;
@@ -16,17 +19,21 @@ function FoodUpload({getImgUrl}){
         }
     }
 
+    // 파일이 인풋되면 프리뷰로 보여주기
     const handleFileInput = (e) => {
         setSelectedFile(e.target.files[0]);
         preview(e);
     };
 
+    // 최종 제출할 이미지의 파일명은 사용자의 ID를 입력하여 고유한 이름으로 변경하기
+    // UID + previewImgUrl(사용자의 로컬 파일명)
     const postImgUrl = () => {
         const UID = 100;
         const joinImgURL = UID + previewImgUrl;
         getImgUrl(joinImgURL);
     }
 
+    // 부모 컴포넌트로 이미지를 전달하기
     const handleUpload = () => {
         postImgUrl();
     }
@@ -48,6 +55,5 @@ function FoodUpload({getImgUrl}){
 FoodUpload.propTypes = {
     getImgUrl: PropTypes.func.isRequired,
 };
-
 
 export default FoodUpload;

fbd949301ab7b41cb77410978a093cf78f9cb03b
Raihyul <tlompy1104@gmail.com>
2021-11-22 19:55:46 +0900

    [#7] fixed: Restore youtube api key, Add comments
    - Youtube API KEY 임시로 사용하던 것 삭제
    - 코드에 대한 주석 & TODO 추가
    
    211122 myLee

--- src/api/food.jsx
+++ src/api/food.jsx
@@ -2,10 +2,7 @@ import axios from 'axios';
 import commonAPI from './common';
 
 const YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3';
-// TODO: Youtube API 키 원래대로 돌리기
-// const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
-// const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY_SUB;
-const YOUTUBE_API_KEY = "AIzaSyAu_idjnlqJevCb_K3jcBj8_1pGaFW6FIc";
+const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 const youTubeApi = (url) => async (data) => {
   try {
--- src/components/FoodSearch/FoodRecipes.jsx
+++ src/components/FoodSearch/FoodRecipes.jsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
+// import { useQuery } from 'react-query';
 import PropTypes from 'prop-types';
 import jiseekApi from '../../api';
 
+// 음식 레시피 검색
 const FoodRecipes = ({ food }) => {
   const [recipes, setRecipes] = useState([]);
   useEffect(() => {
     const searchRecipes = async () => {
       try {
         const resultIds = await jiseekApi.getRecipeList({
-          q: `${food} 방법`,
-          videoEmbeddable: true,
-          type: 'video',
+          q: `${food} 방법`, // 검색어, 레시피/방법/만들기 중 방법이 가장 결과가 좋음
+          videoEmbeddable: true, // 다른 페이지 게시 가능 여부
+          type: 'video', // 영상에서 검색
           regionCode: 'KR',
           part: 'snippet',
-          fields: 'items(id(videoId))',
+          fields: 'items(id(videoId))', // 영상 id를 결과값으로 도출
           maxResults: 10,
         });
         const idList = resultIds.map(({ id }) => id.videoId);
@@ -30,22 +32,26 @@ const FoodRecipes = ({ food }) => {
               ),
             );
             const detailList = resultDetails.map((detail) => detail[0]);
-            detailList.map((a) => console.log(a.snippet.title));
+            // TODO: 코드 완료되면 테스트용 console.log 지우기
+            // detailList.map((a) => console.log(a.snippet.title));
             const detailListCleaned = [];
             detailList.map((data) => {
+              // 띄어쓰기를 제거한 영상 제목에 찾고자 하는 음식 명이 정확히 들어있는지 확인
               const checkTitle = data.snippet.title.replace(/\s/g, '');
               if (checkTitle.includes(food)) {
                 detailListCleaned.push(data);
               }
               return detailListCleaned;
             });
+            // 조회수 높은 순으로 정렬
             detailListCleaned.sort(
               (a, b) =>
                 parseInt(b.statistics.viewCount, 10) -
                 parseInt(a.statistics.viewCount, 10),
             );
-            console.log('cleaned');
-            detailListCleaned.map((a) => console.log(a.snippet.title));
+            // TODO: 코드 완료되면 테스트용 console.log 지우기
+            // console.log('cleaned');
+            // detailListCleaned.map((a) => console.log(a.snippet.title));
             setRecipes(detailListCleaned.slice(0, 4));
           } catch (e) {
             console.log(e);
@@ -74,7 +80,11 @@ const FoodRecipes = ({ food }) => {
 
 export default FoodRecipes;
 
+// 음식 레시피 유튜브 영상을 페이지에 띄우는 부분
+// 검색 결과 확인을 위해 임시로 사용
+// TODO: 디자인 잡히면 지우고 FoodRecipes return문에서 정보만 내보낼 것
 export const YoutubeContent = ({ youtube }) => {
+  // 개행처리 된 부분을 <br /> 태그로 변경
   const descriptionReplaced = youtube.snippet.description.replace(
     /\n/g,
     '<br />',
@@ -82,9 +92,11 @@ export const YoutubeContent = ({ youtube }) => {
   return (
     <div>
       <p>제목 : {youtube.snippet.title}</p>
+      {/* html을 직접 건들기 위해서 dangerouslySetInnerHTML 사용 */}
       <p dangerouslySetInnerHTML={{ __html: descriptionReplaced }} />
       <p>게시일 : {youtube.snippet.publishedAt.slice(0, 10)}</p>
       <p>조회수 : {youtube.statistics.viewCount}</p>
+      {/* TODO: 영상 길이 HH:MM:SS 단위로 변경하기 */}
       <p>영상 길이 : {youtube.contentDetails.duration}</p>
       <iframe
         width="360px"

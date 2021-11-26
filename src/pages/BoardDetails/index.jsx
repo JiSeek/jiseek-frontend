f2faf76732b9c5c8b19c9aafe3df7fef444fa6da
yeah <yeahleem2@gmail.com>
2021-11-27 01:19:06 +0900

    [#10] style : Change constants name 211127 yeLim

--- src/pages/BoardDetails/index.jsx
+++ src/pages/BoardDetails/index.jsx
@@ -3,8 +3,8 @@ import { useLocation } from 'react-router-dom';
 import { BoardUpload } from '../../components/BoardDetails';
 
 const BoardDetailPage = () => {
-  const navigate = useLocation();
-  console.log('디테일', navigate);
+  const location = useLocation();
+  console.log('디테일', location);
 
   return 
     <div>

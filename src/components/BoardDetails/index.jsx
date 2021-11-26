bc5d193a1859ca00d1cfaf1b4ffa3b107aea8ecf
yeah <yeahleem2@gmail.com>
2021-11-27 01:11:53 +0900

    [#10] Modified index file of BoardDetail 211127 yeLim

--- src/components/BoardDetails/index.jsx
+++ src/components/BoardDetails/index.jsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import BoardUpload from './BoardUpload';
-
-function BoardIndex(){
-    const queryClient = new QueryClient();
-    return (
-        <QueryClientProvider client={queryClient}>
-            <BoardUpload />
-        </QueryClientProvider>
-    );
-}
-
-export default BoardIndex;
+export { default as BoardUpload } from './BoardUpload';
--- src/pages/BoardDetails/index.jsx
+++ src/pages/BoardDetails/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { BoardUpload } from '../../components/BoardDetails';
 
 const BoardDetailPage = () => {
   const navigate = useLocation();
   console.log('디테일', navigate);
 
-  return <div>BoardDetails</div>;
+  return 
+    <div>
+      BoardDetails
+      <BoardUpload />
+    </div>;
 };
 
 export default BoardDetailPage;

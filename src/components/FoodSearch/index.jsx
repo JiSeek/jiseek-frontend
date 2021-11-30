b1248e8f6912e68321b94aca3f5aec827c99baf1
Ji Yoon Park <jyp0293@gmail.com>
2021-11-30 15:05:22 +0900

    [#24] feat: Fetch foodInfo for chart and table

--- src/components/FoodSearch/index.jsx
+++ src/components/FoodSearch/index.jsx
@@ -2,3 +2,4 @@ export { default as FoodSearchBar } from './FoodSearchBarContainer';
 export { default as FoodRecipes } from './FoodRecipesContainer';
 export { default as SearchTab } from './SearchTabContainer';
 // export { default as FoodSearch } from './FoodSearch';
+export { default as Nutrition } from './Nutrition';

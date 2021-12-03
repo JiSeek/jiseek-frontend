096f24a9d3b699f63909a27c94d282eb02d0fb4b
yeah <yeahleem2@gmail.com>
2021-12-03 21:11:00 +0900

    [#18] feat : Add board feature
    211203 yeLim

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'; 
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom'; 
 import { useQuery } from 'react-query';
 import jiseekApi from '../../api';
 import { boardKeys } from '../../constants';
@@ -8,26 +8,43 @@ import { boardKeys } from '../../constants';
 function Board() {
     const navigate = useNavigate();
     const { isLoading, isError, error, data: boardList } = useQuery(boardKeys.superior, jiseekApi.get()); 
+    
     if (isError) {
-        console.log(error);
-        navigate('/board/');
+        return (
+            <>
+                <div>{error}</div>
+                <NavLink to={navigate(-1)}>
+                    <button type='button'>
+                        이전 페이지로 돌아가기
+                    </button>
+                </NavLink>
+            </>
+        );
     }
+
+    const handleClick = () => {
+        // if logged in
+        navigate('/board/upload');
+        // else 모달창 '로그인이 필요한 서비스입니다'
+    }
+
     return (
         <>
             { isLoading ? 
                 <div>로딩 아이콘</div>
                 :
                 <GridContainer>
-                    { boardList && boardList.map( (board) => (
-                        <li className='items' key={board.id}>
-                            <NavLink to={`/board/${board.id}`}>
-                                <img src={board.src} alt='이미지' />
-                            </NavLink>
-                        </li>
+                    { boardList && Object.entries(boardList).map( (board) => (
+                        <NavLink to={`/board/${board.id}`}>    
+                            <div className='items' key={board.id}>
+                                <img src={board.photo} alt='이미지' />
+                            </div>
+                        </NavLink>
                     ))
                     }
                 </GridContainer>
             }
+            <button type='button' onClick={handleClick}>글쓰기</button>
         </>
     );
 }

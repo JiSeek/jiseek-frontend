271b9f5b7af2a193f38e44fbdf846bd60bfda5b7
Jinseong <aimer120@nate.com>
2021-12-12 14:15:37 +0900

    [#] refactor: Refactor board main, details page
    211212 JS

--- src/components/BoardDetails/BoardDetails.jsx
+++ src/components/BoardDetails/BoardDetails.jsx
@@ -1,17 +1,74 @@
 import React from 'react';
-import BoardDetailsContainer from './BoardDetailsContainer';
-import Comment from './Comment';
+import PropTypes, { number, string } from 'prop-types';
+import { Link, useLocation } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import CommentsContainer from './CommentsContainer';
+import { getLocaleDate } from '../../utils';
+import { LikeButton } from '../common';
+import { boardKeys } from '../../constants';
 
-function BoardDetails() {
+const BoardDetails = ({ user, post, onDelete }) => {
+  const location = useLocation();
+  const { t, i18n } = useTranslation();
+  const from = location.state?.from?.pathname || '..';
+
   return (
     <div>
-      {/* 게시판 디테일 컨테이너 */}
-      <BoardDetailsContainer />
-
+      <Link to={from}>이전</Link>
+      <img src={post?.photo} alt="게시글 이미지" />
+      <div>
+        <span>{post?.user?.name}</span>
+        {user?.id === post?.user?.pk && (
+          <span>
+            <button type="button" onClick={onDelete}>
+              게시판 삭제
+            </button>
+            <Link
+              to="./modify"
+              state={{ photo: post?.photo, content: post?.content }}
+            >
+              게시판 수정
+            </Link>
+          </span>
+        )}
+      </div>
+      <p>{post?.content}</p>
+      <div>
+        <span>{getLocaleDate(post?.created, i18n.language)}</span>
+        {post?.created.slice(0, 19) !== post?.modified.slice(0, 19) && (
+          <span>{t('boardBeModified')}</span>
+        )}
+      </div>
+      <div>
+        <LikeButton
+          type="board"
+          data={{
+            pk: post?.id,
+            photo: post.photo,
+            content: post?.content,
+            created: post?.created,
+          }}
+          like={post?.is_fav}
+          refreshKey={boardKeys.postById(post?.id)}
+        />
+        <span>{post?.count}</span>
+      </div>
       <hr />
-      {/* 댓글 목록 */}
-      <Comment />
+      <CommentsContainer postId={post?.id} user={user} />
     </div>
   );
-}
+};
+
+BoardDetails.propTypes = {
+  user: PropTypes.objectOf(PropTypes.oneOfType([number, string])),
+  post: PropTypes.objectOf(PropTypes.any),
+  onDelete: PropTypes.func,
+};
+
+BoardDetails.defaultProps = {
+  user: { id: -1, token: null },
+  post: {},
+  onDelete: null,
+};
+
 export default BoardDetails;
---
+++ src/components/BoardDetails/CommentsContainer.jsx
@@ -0,0 +1,169 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import PropTypes, { string, number } from 'prop-types';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { toast } from 'react-toastify';
+import { useTranslation } from 'react-i18next';
+import { useModalContext } from '../../contexts';
+import jiseekApi from '../../api';
+import { boardKeys, mutationKeys } from '../../constants';
+import Comments from './Comments';
+
+const CommentsContainer = ({ postId, user }) => {
+  const { t } = useTranslation();
+  const openModal = useModalContext();
+  const queryClient = useQueryClient();
+  const [text, setText] = useState('');
+  const [modifiedText, setModifiedText] = useState('');
+  const [onUpdateComment, setOnUpdateComment] = useState(-1);
+
+  // 댓글 조회 기능 (R)
+  const { data: comments, status } = useQuery(
+    boardKeys.commentsByPostId(postId),
+    jiseekApi.get(),
+    {
+      refetchOnWindowFocus: true,
+      staleTime: 600000,
+    },
+  );
+
+  // 댓글 생성 기능 (C)
+  const { mutate: createComment } = useMutation(
+    (content) =>
+      jiseekApi.post(`/boards/${postId}/comments/`, {
+        token: user.token,
+        content,
+      }),
+    {
+      mutationKey: mutationKeys.commentCreate,
+      onMutate: async () => {
+        await queryClient.cancelQueries(boardKeys.commentsByPostId(postId));
+      },
+      onSuccess: () => {
+        setText('');
+        toast.success(t('boardCreateSucc'));
+      },
+      onError: () => toast.error(t('boardCreateErr')),
+      onSettled: () =>
+        queryClient.invalidateQueries(boardKeys.commentsByPostId(postId)),
+    },
+  );
+
+  // 댓글 255자로 제한
+  useEffect(() => {
+    if (text.length > 255) {
+      toast.warn(t('boardLimitedText'));
+      setText(text.slice(0, 255));
+    }
+    if (modifiedText.length > 255) {
+      toast.warn(t('boardLimitedText'));
+      setText(modifiedText.slice(0, 255));
+    }
+  }, [text, modifiedText, t]);
+
+  // 댓글 삭제 기능 (D)
+  const { mutate: deleteComment } = useMutation(
+    (id) =>
+      jiseekApi.delete(`/boards/${postId}/comments/${id}/`, {
+        token: user.token,
+      }),
+    {
+      mutationKey: mutationKeys.commentDelete,
+      onMutate: async () => {
+        await queryClient.cancelQueries(boardKeys.commentsByPostId(postId));
+      },
+      onSuccess: () => toast.success(t('boardDeleteSucc')),
+      onError: () => toast.error(t('boardDeleteErr')),
+      onSettled: () => {
+        queryClient.invalidateQueries(boardKeys.commentsByPostId(postId));
+      },
+    },
+  );
+
+  // 댓글 수정 기능 (U)
+  const { mutate: updateComment } = useMutation(
+    ({ id, content }) =>
+      jiseekApi.put(`/boards/${postId}/comments/${id}/`, {
+        token: user.token,
+        content,
+      }),
+    {
+      mutationKey: mutationKeys.commentUpdate,
+      onMutate: async () => {
+        await queryClient.cancelQueries(boardKeys.commentsByPostId(postId));
+      },
+      onSuccess: () => {
+        setOnUpdateComment(-1);
+        toast.success(t('boardUpdateSucc'), { toastId: 'TODO' });
+      },
+      onError: () => toast.error(t('boardUpdateErr'), { toastId: 'TODO' }),
+      onSettled: () =>
+        queryClient.invalidateQueries(boardKeys.commentsByPostId(postId)),
+    },
+  );
+
+  const handleCreate = useCallback(
+    (e) => {
+      e.preventDefault();
+      createComment(text);
+    },
+    [createComment, text],
+  );
+
+  const handleUpdate = useCallback(
+    (id) => {
+      if (onUpdateComment !== id) {
+        setOnUpdateComment(id);
+        return;
+      }
+
+      if (!modifiedText) {
+        toast.warn(t('boardUpdateNone'));
+        return;
+      }
+      updateComment({ id, content: modifiedText });
+    },
+    [updateComment, modifiedText, t, onUpdateComment],
+  );
+
+  const handleDeleteCancel = useCallback(
+    (id) => {
+      if (onUpdateComment === id) {
+        setOnUpdateComment(-1);
+        return;
+      }
+
+      openModal(t('boardDeleteNor'), 'select', {
+        yes: () => deleteComment(id),
+        no: () => {},
+      });
+    },
+    [openModal, deleteComment, t, onUpdateComment],
+  );
+
+  return (
+    <Comments
+      userId={user?.id}
+      status={status}
+      comments={comments}
+      onUpdateComment={onUpdateComment}
+      text={text}
+      setText={setText}
+      setModifiedText={setModifiedText}
+      onCreate={handleCreate}
+      onUpdate={handleUpdate}
+      onDeleteCancel={handleDeleteCancel}
+    />
+  );
+};
+
+CommentsContainer.propTypes = {
+  postId: PropTypes.number,
+  user: PropTypes.objectOf(PropTypes.oneOfType([number, string])),
+};
+
+CommentsContainer.defaultProps = {
+  postId: -1,
+  user: { id: -1, token: null },
+};
+
+export default CommentsContainer;

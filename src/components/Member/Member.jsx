c1c7dd68ad0309ee804f6f05399e6c59e1a1c980
Raihyul <tlompy1104@gmail.com>
2021-12-12 18:24:16 +0900

    [#39] design: Design member page - 소개 페이지 한/영 번역 추가 211212 myLee

--- src/components/Member/Member.jsx
+++ src/components/Member/Member.jsx
@@ -1,174 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
-import { SiGithub } from 'react-icons/si';
-import {
-  Yerim,
-  Jihun,
-  Jiyun,
-  Minyeong,
-  Jinseong,
-  Jeonghyeon,
-  Velog,
-} from '../../assets/images/images';
+import { useTranslation } from 'react-i18next';
+import { SiGithub, SiBloglovin } from 'react-icons/si';
+import { parseParagraph } from '../../utils';
 
-const Member = () => (
-  <MemberOutlet>
-    <EachMember>
-      <img src={Yerim} alt="Profile of Yerim" />
-      <div>
-        <span>고예림</span>
-        <span>
-          <a href="https://github.com/yeahLim" target="_blank" rel="noreferrer">
-            <SiGithub />
-          </a>
-        </span>
-      </div>
-      <p>
-        <span>Frontend</span>
-        <span>
-          #커뮤니티, 디테일, 업로드 페이지 구성 및 기능 구현 #게시글 CRUD 기능
-          구현
-          <br />
-          #댓글 CRUD 기능 구현 #etc
-        </span>
-        <span>
-          엘리스라는 프로그램을 통해 처음으로 웹 분야를 배우게 되었습니다.
-          프론트엔드 직무는 다른 분야와 달리 화면에서 실시간으로 제 코드가
-          반영되어 큰 재미를 느끼고 있습니다. 이번 프로젝트를 하면서 React
-          Query와 container/presentational 패턴에 대해 배울 수 있었습니다.
-          앞으로 효율적인 구조와 편리한 유저경험을 위해 노력하는 개발자가
-          되겠습니다!
-        </span>
-      </p>
-      <div />
-    </EachMember>
-    <EachMember>
-      <img src={Jeonghyeon} alt="Profile of Yerim" />
-      <div>
-        <span>고정현</span>
-        <span>
-          <a
-            href="https://github.com/kojh0111"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <SiGithub />
-          </a>
-        </span>
-      </div>
-      <p>
-        <span>Backend</span>
-        <span>
-          #음식 검색 관련 API 구현 #게시판 관련 REST API 구현 #서비스 배포 #etc
-        </span>
-        <span>새로운 것에 도전하는 게 즐거운 백엔드 개발자입니다.</span>
-      </p>
-      <div />
-    </EachMember>
-    <EachMember>
-      <img src={Jihun} alt="Profile of Yerim" />
-      <div>
-        <span>김지훈</span>
-        <span>
-          <a
-            href="https://github.com/combiJihoon"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <SiGithub />
-          </a>
-          <a
-            href="https://velog.io/@combi_jihoon"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={Velog} alt="velog logo" width={28} />
-          </a>
-        </span>
-      </div>
-      <p>
-        <span>Backend</span>
-        <span>
-          #자체 유저 인증 및 소셜 인증(네이버&카카오&구글) 관련 API 구현 <br />
-          #마이 페이지 및 좋아요 관련 API 구현 #서비스 배포 #etc
-        </span>
-        <span>
-          화학 연구원으로 일하다 실험 자동화와 효율성에 관심을 갖고 개발자에
-          입문하게 되었습니다. 필요한 데이터를 전달하고 배포 환경을 구축하는
-          일이 재밌는 백엔드 개발자입니다!
-        </span>
-      </p>
-      <div />
-    </EachMember>
-    <EachMember>
-      <img src={Jiyun} alt="Profile of Yerim" />
+const Member = () => {
+  const { t } = useTranslation();
 
-      <div>
-        <span>박지윤</span>
-        <span>contact</span>
-      </div>
-      <p>
-        <span>AI</span>
-        <span>#etc</span>
-        <span>간단한 본인 소개 글</span>
-      </p>
-      <div />
-    </EachMember>
-    <EachMember>
-      <img src={Minyeong} alt="Profile of Yerim" />
-      <div>
-        <span>이민영</span>
-        <span>
-          <a href="https://github.com/Raihyul" target="_blank" rel="noreferrer">
-            <SiGithub />
-          </a>
-        </span>
-      </div>
-      <p>
-        <span>Frontend</span>
-        <span>#외부 API 연동 #UX/UI 디자인 #etc</span>
-        <span>
-          기계 설계에서 웹 개발로 전향하여 프론트엔드 개발자를 희망하고
-          있습니다! <br /> 사용자 경험성을 최우선으로 하고 있으며, UX/UI
-          디자인을 즐기고 있습니다:)
-        </span>
-      </p>
-      <div />
-    </EachMember>
-    <EachMember>
-      <img src={Jinseong} alt="Profile of Yerim" />
-      <div>
-        <span>전진성</span>
-        <span>
-          <a
-            href="https://github.com/notCoderJ"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <SiGithub />
-          </a>
-        </span>
-      </div>
-      <p>
-        <span>Frontend</span>
-        <span>
-          #음식 검색(이름, 사진) 페이지 기능 구현 #커뮤니티 페이지 기능 리펙토링
-          <br />
-          #사용자 관련 기능(로그인, 회원가입, 마이 페이지) 구현 #etc
-        </span>
-        <span>
-          지난 3년간 시스템 개발자로 일하다가 즉각적인 결과물 확인과 다양한
-          사용자에게 서비스를 제공할 수 있다는 매력에 빠져 프론트엔드 분야에
-          도전하게 되었습니다. <br />
-          코드 구조화를 좋아하고 사용자가 서비스를 사용하며 불쾌감을 느끼지
-          않도록 세세한 부분까지 신경쓰는 프론트엔드 개발자가 되기 위해 노력하고
-          있습니다!
-        </span>
-      </p>
-      <div />
-    </EachMember>
-  </MemberOutlet>
-);
+  return (
+    <MemberOutlet>
+      {[
+        'MemberYRGo',
+        'MemberJHKoh',
+        'MemberJHKim',
+        'MemberJYPark',
+        'MemberMYLee',
+        'MemberJSJeon',
+      ].map((name) => (
+        <EachMember>
+          <img src={t(`${name}.profile`)} alt={t(`${name}.name`)} width={30} />
+          <div>
+            <span>{t(`${name}.name`)}</span>
+
+            <span>
+              <a href={t(`${name}.git`)} target="_blank" rel="noreferrer">
+                <SiGithub />
+              </a>
+              {t(`${name}.blog`) && (
+                <a href={t(`${name}.blog`)} target="_blank" rel="noreferrer">
+                  <SiBloglovin />
+                </a>
+              )}
+            </span>
+          </div>
+          <p>
+            <span>{t(`${name}.part`)}</span>
+            {parseParagraph(t(`${name}.role`))}
+            {parseParagraph(t(`${name}.intro`))}
+          </p>
+          <div />
+        </EachMember>
+      ))}
+    </MemberOutlet>
+  );
+};
 
 const MemberOutlet = styled.ul`
   display: flex;
---
+++ src/lang/Members/en_members.js
@@ -0,0 +1,72 @@
+import {
+  Yerim,
+  Jeonghyeon,
+  Jihun,
+  Jiyun,
+  Minyeong,
+  Jinseong,
+} from '../../assets/images/images';
+
+const EN_MEMBERS = {
+  MemberYRGo: {
+    name: 'Yerim Go',
+    profile: Yerim,
+    git: 'https://github.com/yeahLim',
+    blog: '',
+    part: 'Frontend',
+    role: '#Community, Detail, Upload Page Configuration and Function Implementation #Posted CRUD Function Implementation \n #Comment CRUD Function Implementation #etc',
+    intro:
+      'Through the Elice AI Track, I learned the web field for the first time. Unlike other fields, front-end jobs are having a lot of fun because my code is reflected in real time on the screen. During this project, I was able to learn about React Query and container/presentational patterns. In the future, I will become a developer who strives for an efficient structure and convenient user experience!',
+  },
+  MemberJHKoh: {
+    name: 'Jeonghyeon Koh',
+    profile: Jeonghyeon,
+    git: 'https://github.com/kojh0111',
+    blog: '',
+    part: 'Backend',
+    role: '#Food search-related API implementation #Service deployment \n #REST API implementation related to bulletin boards #etc',
+    intro: "I'm a back-end developer who enjoys challenging new things.",
+  },
+  MemberJHKim: {
+    name: 'Jihun Kim',
+    profile: Jihun,
+    git: 'https://github.com/combiJihoon',
+    blog: 'https://velog.io/@combi_jihoon',
+    part: 'Backend',
+    role: '#Implement API related to self-user authentication and social authentication (Naver & Kakao & Google) \n #Implement API related to my page and likes #Service deployment #etc',
+    intro:
+      'While working as a chemistry researcher, I became interested in experimental automation and efficiency and entered the developer. \n I am a back-end developer that have fun with delivering necessary data and building a distribution environment!',
+  },
+  MemberJYPark: {
+    name: 'Jiyoon Park',
+    profile: Jiyun,
+    git: 'https://github.com/highballl',
+    blog: 'https://highballl.oopy.io',
+    part: 'AI',
+    role: '#이미지 감지 및 분류(Yolov4), 이미지 데이터 전처리(Python-OpenCV) \n #이미지 리사이징, 바운딩 박스 드로잉(AWS S3+Lambda, Node.js-sharp) \n #결과 차트 및 테이블 기능 구현(React), #기타',
+    intro:
+      '2021년 1월부터 개발자가 되기 위해 공부를 시작했습니다. 의류학, 미디어아트, 언론정보학(석사 수료) 등 다양한 전공을 거쳐 최근까지는 대안교육 과학강사로 일을 했습니다. 무언가를 실제로 구현한다는 차원에서 매체 전반에 대한 관심이 항상 있었습니다. 학부 때 미디어아트를 복수전공하며 아두이노에 대해 알게 되었고 그때부터 코딩에 대한 관심이 생겼습니다. 이후 과학 수업을 하면서 아두이노, 브레드보드 등을 종종 교구로도 활용했고, 그러면서 앞으로 개인 작업 및 생업 전반의 영역에서 프로그래밍 언어로 실제 서비스/제품 구현을 할 수 있으면 좋겠다고 생각하여 공부를 시작하게 되었습니다.',
+  },
+  MemberMYLee: {
+    name: 'Minyeong Lee',
+    profile: Minyeong,
+    git: 'https://github.com/Raihyul',
+    blog: '',
+    part: 'Frontend',
+    role: '#Interworking with external APIs #UX/UI Design #etc',
+    intro:
+      'I want to become a front-end developer by shifting from mechanical design to web development! \n I put user experience first and enjoy UX/UI design:)',
+  },
+  MemberJSJeon: {
+    name: 'Jinseong Jeon',
+    profile: Jinseong,
+    git: 'https://github.com/notCoderJ',
+    blog: '',
+    part: 'Frontend',
+    role: '#Implement Food search(by name, by photo) page function #Community page function re-factoring \n #Implemented user-related functions (sign in, sign up, my page) #etc',
+    intro:
+      'After working as a system developer for the past three years, I fell in love with the charm of checking the results immediately and providing services to various users, and challenged the front-end field. \n 코드 구조화를 좋아하고 사용자가 서비스를 사용하며 불쾌감을 느끼지 않도록 세세한 부분까지 신경쓰는 프론트엔드 개발자가 되기 위해 노력하고 있습니다!',
+  },
+};
+
+export default EN_MEMBERS;
